39c646a3397694daeb26c4653809828930cc1736
Biswajit <biswajeet82498@gmail.com>
2024-08-15 14:26:26 +0530

    Data Transformation

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ from src.Student_Performance_Indicator.logger import logging
 from src.Student_Performance_Indicator.exception import CustomException
 from src.Student_Performance_Indicator.components.data_ingestion import DataIngestion
 from src.Student_Performance_Indicator.components.data_ingestion import DataIngestionConfig
+from src.Student_Performance_Indicator.components.data_transformation import DataTransformation, DataTransformationConfig
+# from src.Student_Performance_Indicator.components.data_transformation import DataTransformationConfig
+
 
 import sys
 
@@ -11,8 +14,15 @@ if __name__=="__main__":
 
 
     try:
+        # data_ingestion.initiate_data_ingestion()
         data_ingestion = DataIngestion()
-        data_ingestion.initiate_data_ingestion()
+        train_data_path,test_data_path = data_ingestion.initiate_data_ingestion()
+
+        # config = DataTransformationConfig()
+        # data_transformation_config = DataTransformationConfig
+        data_transformation=DataTransformation()
+        # train_arr,test_arr = data_transformation.initiate_data_transormation(train_data_path, test_data_path)
+        data_transformation.initiate_data_transormation(train_data_path, test_data_path)
 
         
     except Exception as e:
